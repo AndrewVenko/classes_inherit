80526f9a3759ab1298671b5b9c9cc3b831572f1b
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-04-18 09:09:53 +0300

    Add files classes

---
+++ test/Charecter_test.js
@@ -0,0 +1,24 @@
+import Character from '../src/Character';
+
+// Проверяем класс Character
+describe('Character', () => {
+  it('should create a new character with valid name', () => {
+    const character = new Character('John');
+    const objectCharacter = {
+      name: 'John',
+      health: 100,
+      level: 1,
+    };
+
+    expect(character).toEqual(objectCharacter);
+  });
+
+  it('should throw an error if name length is less than 2', () => {
+    expect(() => new Character('')).toThrow('Ошибка! Имя должно содержать min 2, max 10 символов!');
+    expect(() => new Character('A')).toThrow('Ошибка! Имя должно содержать min 2, max 10 символов!');
+  });
+
+  it('should throw an error if name length is greater than 10', () => {
+    expect(() => new Character('JohnSmithJr')).toThrow('Ошибка! Имя должно содержать min 2, max 10 символов!');
+  });
+});
